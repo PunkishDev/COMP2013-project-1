f4528ba2e426b35645e752cfef048fe9ae1f1110
JordynHam <jordyn_business@outlook.com>
2025-10-16 15:03:43 -0400

    Modified nav bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,9 @@
-export default function NavBar({username = "username"}) {
-    return <div className="NavBar">
-        <p className="NavUser">Hello {username}</p>
-        <h1 className="NavTitle">C̶̢͕̯̭̫̞̏u̵̪͓͚̱̔̐̕r̷̨͗̿̊̀͜ͅs̴͚͂ȩ̸̱͐͒̔̾̿̓d̸͇̥̍̇̔̿͘ ̸̲̅̌̓͋͂G̶̜͕̤̳̬̥̀̽̓̇̀̋r̷̢͎͉̣̟̿̅̕͝͝o̵͉̖̍̎̌̏c̶̱̿͑͋e̴̻̝̫̘̐́͐r̷̙͓͔̲̲̄̌i̵̩̠̞͆̂͊̐̾̾e̸͇̮̣̺͇̅̈́̓s̴̢̃͝</h1>
-        <h3 className="NavCart">🛒</h3>
+export default function NavBar({ username = "username" }) {
+  return (
+    <div className="NavBar">
+      <p className="NavUser">Hello {username}</p>
+      <h1 className="NavTitle">C̶̢͕̯̭̫̞̏u̵̪͓͚̱̔̐̕r̷̨͗̿̊̀͜ͅs̴͚͂ȩ̸̱͐͒̔̾̿̓d̸͇̥̍̇̔̿͘ ̸̲̅̌̓͋͂G̶̜͕̤̳̬̥̀̽̓̇̀̋r̷̢͎͉̣̟̿̅̕͝͝o̵͉̖̍̎̌̏c̶̱̿͑͋e̴̻̝̫̘̐́͐r̷̙͓͔̲̲̄̌i̵̩̠̞͆̂͊̐̾̾e̸͇̮̣̺͇̅̈́̓s̴̢̃͝</h1>
+      <img src="src/assets/cart-empty.png" alt="" className="NavCart" />
     </div>
+  );
 }
